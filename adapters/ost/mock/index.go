2431eb5835329037ad8a6c8d8546231d4234d0a2
mdb <dauren.asir@gmail.com>
2023-05-26 10:18:39 +0600

    some changes

---
+++ adapters/ost/mock/index.go
@@ -0,0 +1,29 @@
+package mock
+
+import (
+	"bytes"
+	"io"
+)
+
+type St struct {
+}
+
+func New() (*St, error) {
+	return &St{}, nil
+}
+
+func (o *St) CreateBucket(name string) error {
+	return nil
+}
+
+func (o *St) PutObject(bucketName, name string, data io.Reader, contentType string) error {
+	return nil
+}
+
+func (o *St) GetObject(bucketName, name string) (io.ReadCloser, error) {
+	return io.NopCloser(bytes.NewReader([]byte{})), nil
+}
+
+func (o *St) RemoveObject(bucketName, name string) error {
+	return nil
+}
